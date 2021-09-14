1b2428087cddd450fb5fd53fe74efc913299e12c
Abhi <mac@Abhis-MacBook-Air.local>
2021-09-14 10:46:47 +0530

    first commit

---
+++ APICallingUsingMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APICallingUsingMVVM
+//
+//  Created by Abhi on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
